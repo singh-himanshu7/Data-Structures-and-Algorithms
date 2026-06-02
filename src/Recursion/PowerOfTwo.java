a50efed718e9f031f21d110debc67ce3126bb81f
singh-himanshu7 <himanshu7403@gmail.com>
2026-06-02 12:42:39 +0530

    Solution Leetcode 231

--- src/Recursion/PowerOfTwo.java
+++ src/Recursion/PowerOfTwo.java
@@ -1,5 +1,16 @@
 package Recursion;
 
 public class PowerOfTwo {
-
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(16));
+    }
+    public static boolean isPowerOfTwo(int n) {
+        if (n==1){
+            return true;
+        }
+        if (n<=0 || n % 2 != 0) {
+            return false;
+        }
+        return isPowerOfTwo(n/2) ;
+    }
 }
