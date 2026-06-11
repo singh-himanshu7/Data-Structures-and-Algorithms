c1ae125507228d3672f33cd152bedd5da8335dcc
singh-himanshu7 <himanshu7403@gmail.com>
2026-06-11 13:55:53 +0530

    Solution Of LeetCode 33

---
+++ src/Searching/problemsOnBinarySearch/RotatedSortedArray.java
@@ -0,0 +1,31 @@
+package Searching.problemsOnBinarySearch;
+
+public class RotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int st = 0;
+        int end = nums.length - 1;
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // Left
+            if (nums[st] <= nums[mid]) {
+                if (nums[st] <= target && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    st = mid + 1;
+                }
+            }
+            // Right
+            else {
+                if (nums[mid] < target && target <= nums[end]) {
+                    st = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
