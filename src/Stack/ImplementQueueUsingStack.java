a577984291d5cb3943a66a7e7c789d9bed56a261
singh-himanshu7 <himanshu7403@gmail.com>
2026-06-20 15:51:23 +0530

    Solution Of LeetCode 232

---
+++ src/Stack/ImplementQueueUsingStack.java
@@ -0,0 +1,40 @@
+package Stack;
+import java.util.Stack;
+
+public class ImplementQueueUsingStack {
+    Stack<Integer> first;
+    Stack<Integer> second;
+    public ImplementQueueUsingStack() {
+        first = new Stack<>();
+        second = new Stack<>();
+    }
+    public void push(int x) {
+        first.push(x);
+    }
+
+    public int pop() {
+        while(!first.isEmpty()){
+            second.push((first.pop()));
+        }
+        int removed = second.pop();
+        while (!second.isEmpty()){
+            first.push(second.pop());
+        }
+        return removed;
+    }
+
+    public int peek() {
+        while(!first.isEmpty()){
+            second.push((first.pop()));
+        }
+        int peeked = second.peek();
+        while (!second.isEmpty()){
+            first.push(second.pop());
+        }
+        return peeked;
+    }
+
+    public boolean empty() {
+        return first.isEmpty();
+    }
+}
