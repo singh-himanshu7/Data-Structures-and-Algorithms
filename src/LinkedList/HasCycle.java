663de814245e3f2667eeed4e77c5202357dd5cc0
singh-himanshu7 <himanshu7403@gmail.com>
2026-07-21 08:22:25 +0530

    Solution OF Leetcode 141

---
+++ src/LinkedList/HasCycle.java
@@ -0,0 +1,27 @@
+package LinkedList;
+//141
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class HasCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
