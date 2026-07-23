429cb4d3f5d212d76f767d22bc9208db8e9fb662
Himanshu Singh <himanshu7403@gmail.com>
2026-07-23 21:42:45 +0530

    Solution Of LeetCode 142

---
+++ src/LinkedList/LinkedListCycleII.java
@@ -0,0 +1,33 @@
+package LinkedList;
+//142
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != head) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+
+        return null;
+    }
+}
