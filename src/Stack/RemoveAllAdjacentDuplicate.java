81831d918ff769f983d8455dcd6309a9884ddb41
Himanshu Singh <himanshu7403@gmail.com>
2026-08-24 11:40:36 +0530

    Solution of Leetcode 1047

--- src/Stack/RemoveAllAdjacentDuplicate.java
+++ src/Stack/RemoveAllAdjacentDuplicate.java
@@ -1,8 +1,31 @@
 package Stack;
+//1047
+import java.util.Stack;
 
 class RemoveAllAdjacentDuplicate {
-    public String removeDuplicates(String s) {
+    public static String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        stack.push(s.charAt(0));
+        for(int i = 1; i<s.length();i++){
+            if(stack.isEmpty()){
+                stack.push(s.charAt(i));
+            }
+            else if( s.charAt(i)==stack.peek()){
+                stack.pop();
+            }
+            else {
+                stack.push(s.charAt(i));
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
 
-        return "";
+        return sb.reverse().toString();
+    }
+    public static void main(String[] args) {
+        String s = "abbaca";
+        System.out.println(removeDuplicates(s));
     }
 }
