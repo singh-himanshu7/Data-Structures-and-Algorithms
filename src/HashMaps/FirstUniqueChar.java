a60d5196174a024f8c9b2f5ccfd48d7177a23358
Himanshu Singh <himanshu7403@gmail.com>
2026-09-21 10:33:22 +0530

    Solution of Leetcode 387

---
+++ src/HashMaps/FirstUniqueChar.java
@@ -0,0 +1,18 @@
+package HashMaps;
+import java.util.HashMap;
+//387
+class FirstUniqueChar {
+    public int firstUniqChar(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(int i = 0 ; i < s.length() ; i++){
+            if(map.get(s.charAt(i))==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
